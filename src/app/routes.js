69539fb6844b1fc23933a97cd304a070e506f1e8
Justin Peterson <jmp3833@rit.edu>
2014-10-22 11:24:39 -0400

    Added individual page routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,7 +1,28 @@
 // app/routes.js
 
 module.exports = function(app) {
+	
 	//Frontend Routes
+	app.get('/events', function(req, res) {
+		res.sendfile('./public/views/events.html');
+	});
+
+	app.get('/forum', function(req, res) {
+		res.sendfile('./public/views/forum.html');
+	});
+
+	app.get('/inviteFriends', function(req, res) {
+		res.sendfile('./public/views/inviteFriends.html');
+	});
+
+	app.get('/joinEvent', function(req, res) {
+		res.sendfile('./public/views/joinEvent.html');
+	});
+
+	app.get('/eventProfile', function(req, res) {
+		res.sendfile('./public/views/singleEvent.html');
+	});
+
 	app.get('*', function(req, res) {
 		res.sendfile('./public/views/index.html');
 	});
