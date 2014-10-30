5c087b8100ee56b301bb1c289d8b39773c802a8c
Joe Ksiazek <hyzer.jak@gmail.com>
2014-10-30 17:15:00 -0400

    Working googlemap on homepage

--- public/js/googlemaps.js
+++ public/js/googlemaps.js
@@ -1,19 +1,23 @@
-function initialize() {
-    var mapOptions = {
-      center: { lat: 43.083, lng: -77.675},
-      zoom: 14
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-    var marker = new google.maps.Marker({
-        position: { lat: 43.08301, lng: -77.67501},
-        map: map,
-        title:"Sustainalot"
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        map.setZoom(15);
-        map.setCenter(marker.getPosition());
-    });
-}
+var app = angular.module('app');
+app.controller('googlemaps', function($scope) {
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    function init(){
+        var mapOptions = {
+          center: { lat: 43.083, lng: -77.675},
+          zoom: 14
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+        var marker = new google.maps.Marker({
+            position: { lat: 43.08301, lng: -77.67501},
+            map: map,
+            title:"Sustainalot"
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            map.setZoom(15);
+            map.setCenter(marker.getPosition());
+        });
+    }
+    
+    init();
+})
