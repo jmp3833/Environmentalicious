4e373f3fba2a6a76636dc3b3aca2bda24f0c24d1
rxk8345 <rxk8345@g.rit.edu>
2014-10-30 18:33:46 -0400

    Added animation to throwing away trash

--- public/js/about_page.js
+++ public/js/about_page.js
@@ -1,21 +1,59 @@
-function autoResizeDiv(){
 
-	var size = window.innerHeight;
-	if( size < 600){
-		size = '100%'
-	}else{
-		size = size + 'px'
-	}
-	document.getElementById('title_section').style.height = size;
-	document.getElementById('what_we_do_section').style.height = size;
-	document.getElementById('map_it_section').style.height = size;
-	document.getElementById('cleanup_section').style.height = size;
-	document.getElementById('get_involved_section').style.height = size;
+/*
+ *	about_page.js 
+ *  Handles all the parallax scrolling features on this page
+ */
+$(document).ready(function($) {
+
+	// paper throw animation
+	var throwPaperCrumble = {
+		one : {
+				curviness: 1.25,
+				autoRotate: true,
+				values : [  
+					{x: 0,	y: 0},
+					{x: 100,	y: -1000},
+					{x: 200,	y: -800}
+				] 
+		}, two : {
+				curviness: 1.25,
+				autoRotate: true,
+				values : [  
+					{x: 0,	y: 600},
+					{x: -100,	y: -800},
+					{x: -200,	y: -575}
+				] 
+		}, three: {
+				curviness: 1.25,
+				autoRotate: true,
+				values : [  
+					{x: -100,	y: 0},
+					{x: 100,	y: -600},
+					{x: 150,	y: -325}
+				] 
+		}
 }
 
-$( document ).ready(function(){
-	window.onresize = autoResizeDiv;
-	autoResizeDiv();
-});
+	//controller for scroll magic
+	var controller = new ScrollMagic();
 
-var controller = new ScrollMagic();
+	//set tween to have the paper throw
+	var tween1 = new TimelineMax()
+			.add(TweenMax.to($("#crumble1"), 1.5, {css:{bezier:throwPaperCrumble.one}, ease:Linear.easeNone}))
+			.add(TweenMax.to($("#crumble1"), 0.05, {css:{display:'none'}, ease:Linear.easeNone}) )
+			.add(TweenMax.to($("#crumble2"), 1.5, {css:{bezier:throwPaperCrumble.two}, ease:Linear.easeNone}))
+			.add(TweenMax.to($("#crumble2"), 0.05, {css:{display:'none'}, ease:Linear.easeNone}) )
+			.add(TweenMax.to($("#crumble3"), 1.5, {css:{bezier:throwPaperCrumble.three}, ease:Linear.easeNone}))
+			.add(TweenMax.to($("#crumble3"), 0.05, {css:{display:'none'}, ease:Linear.easeNone}) ) ;
+
+
+	var scene1 = new ScrollScene({triggerElement: "#clean_up_title", duration: 700, offset: 150})
+			.setPin("#trigger_bin")
+			.setTween(tween1)
+			.addTo(controller);
+
+
+
+	// scene.addIndicators();
+
+});
